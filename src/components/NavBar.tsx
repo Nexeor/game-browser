f286a53c4f0cdf7eb0ce80aed2b092f2ab4adb50
tima20 <tima20@vt.edu>
2025-01-22 13:55:10 -0500

    Added NavBar skeleton

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src="/Steam Icon.png" boxSize="60px"></Image>
+      <Text>NAVBAR PLACEHOLDER</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
