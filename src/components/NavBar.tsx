0d11bfff495fc02b406170f1bf336e45417637d5
tima20 <tima20@vt.edu>
2025-01-29 16:31:59 -0500

    Added search bar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src="/Steam Icon.png" boxSize="60px"></Image>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
